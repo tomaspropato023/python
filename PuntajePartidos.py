9b374dbe756dd2196801ee844fb25ef593f9a462
Tomas <106850757+Tomson019@users.noreply.github.com>
2024-04-17 10:38:47 -0300

    Ejercicio 4

---
+++ PuntajePartidos.py
@@ -0,0 +1,22 @@
+# Ejercicio 4
+
+print("-----------------------------------------")
+print("Ejercicio 4: Puntaje total de Partidos")
+print("-----------------------------------------")
+
+print("Ingrese el numero de partidos ganados: ")
+PG = int(input())
+print("Ingrese el numero de partidos empatados: ")
+PE = int(input())
+print("Ingrese el numero de partidos perdidos: ")
+PP = int(input())
+
+PPG = PG * 3
+PPE = PE * 1
+PPP = PP * 0
+
+PT = PPG+PPE+PPP
+
+print("\nSalida: ")
+print("-------------------------------")
+print("El puntaje total es ", PT)
