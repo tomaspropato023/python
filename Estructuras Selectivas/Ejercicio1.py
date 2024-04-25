658da47e534238c9a41a44bd04fb7c2851652205
Tomas <106850757+Tomson019@users.noreply.github.com>
2024-04-25 11:44:51 -0300

    Ejercicios Estructuras Selectivas (if)

---
+++ Estructuras Selectivas/Ejercicio1.py
@@ -0,0 +1,5 @@
+#EJ 1 - Estructuras Selectivas
+
+num = int(input("Ingrese un numero: "))
+if num < 100:
+    print("Numero pequeño.")
---
+++ Estructuras Selectivas/Ejercicio2.py
@@ -0,0 +1,9 @@
+#EJ 2 - Estructuras Selectivas
+
+num1 = int(input("Ingrese un numero: "))
+num2 = int(input("Ingrese otro numero: "))
+
+if num1 > num2:
+    print("El numero menor es",num2)
+else:
+    print("El numero menor es",num1)
