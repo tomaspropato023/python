60de7dda20b6004097535be543da8334d2d853d0
Tomas <106850757+Tomson019@users.noreply.github.com>
2024-05-15 10:52:57 -0300

    Update Sueldos.py

--- Estructuras Selectivas/Sueldos.py
+++ Estructuras Selectivas/Sueldos.py
@@ -5,7 +5,7 @@ if sueldo > 0 and sueldo <= 15000:
 elif sueldo >= 15001 and sueldo <= 20000:
     sueldo *= 1.1
     print("Tiene un aumento del 10%. Su salario pasa a ser de $",sueldo)
-elif sueldo >= 20001 and sueldo <= 25001:
+elif sueldo >= 20001 and sueldo <= 25000:
     sueldo *= 1.05
     print("Tiene un aumento del 5%. Su salario pasa a ser de $",sueldo)
 else:
