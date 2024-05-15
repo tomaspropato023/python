1259c40a3ccb8e549f9883bf8ae37aa3238ee5fc
Tomas <106850757+Tomson019@users.noreply.github.com>
2024-05-15 10:50:28 -0300

    Update Sueldos.py

--- Estructuras Selectivas/Sueldos.py
+++ Estructuras Selectivas/Sueldos.py
@@ -1,13 +1,13 @@
 sueldo = int(input("Ingrese su salario actual: "))
 if sueldo > 0 and sueldo <= 15000:
-    sueldoConAumento = sueldo * 1.2
-    print("Tiene un aumento del 20%. Su salario pasa a ser de $",sueldoConAumento)
+    sueldo *= 1.2
+    print("Tiene un aumento del 20%. Su salario pasa a ser de $",sueldo)
 elif sueldo >= 15001 and sueldo <= 20000:
-    sueldoConAumento = sueldo * 1.1
-    print("Tiene un aumento del 10%. Su salario pasa a ser de $",sueldoConAumento)
+    sueldo *= 1.1
+    print("Tiene un aumento del 10%. Su salario pasa a ser de $",sueldo)
 elif sueldo >= 20001 and sueldo <= 25001:
-    sueldoConAumento = sueldo * 1.05
-    print("Tiene un aumento del 5%. Su salario pasa a ser de $",sueldoConAumento)
+    sueldo *= 1.05
+    print("Tiene un aumento del 5%. Su salario pasa a ser de $",sueldo)
 else:
     print("No tiene aumento. Su salario sigue siendo de $", sueldo)
     
