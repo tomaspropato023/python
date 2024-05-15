d541110cd1e39a04afe50acb14026d0a16f98898
Tomas <106850757+Tomson019@users.noreply.github.com>
2024-05-15 10:47:20 -0300

    Update Ejercicio5.py

--- Estructuras Selectivas/Ejercicio5.py
+++ Estructuras Selectivas/Ejercicio5.py
@@ -1,7 +1,7 @@
 #EJ 5 - Estructuras Selectivas
 
 anio = int(input("Ingrese un año: "))
-if (anio % 4 == 0) and (anio % 100 != 0) or (anio % 400 == 0):
+if (anio % 400 == 0) or (anio % 4 == 0) and (anio % 100 != 0):
     print ("El año es bisiesto.")
 else:
     print ("El año no es bisiesto.")
