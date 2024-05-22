58ef6696e57df7a13900b09763674649cec010ed
Tomas <166726520+tomaspropato023@users.noreply.github.com>
2024-05-22 11:48:02 -0300

    Update Clave.py

--- Estructuras Repetitivas/Clave.py
+++ Estructuras Repetitivas/Clave.py
@@ -5,11 +5,11 @@ CLAVE = "eureka"
 claveIngr = str(input("Ingrese la clave: "))
 contador = 0
 
-while (contador < 2) and (claveIngr.casefold() != CLAVE.casefold()):
+while (contador < 2) and (claveIngr.casefold() != CLAVE):
     claveIngr = str(input("Clave incorrecta. Vuelva a ingresar la clave: "))
     contador += 1
 
-if claveIngr.casefold() != CLAVE.casefold():
+if claveIngr.casefold() != CLAVE:
     print("Ha agotado los intentos.")
     StopIteration
 else:
