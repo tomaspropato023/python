ee75385d58c05c6b85847c2455063920b66cc1d7
Tomas <166726520+tomaspropato023@users.noreply.github.com>
2024-05-29 11:55:25 -0300

    Update Calificaciones.py

--- Estructuras Repetitivas/Calificaciones.py
+++ Estructuras Repetitivas/Calificaciones.py
@@ -11,9 +11,12 @@ while (nombreAlumno != ""):
     nota2 = int(input("Nota de problemas: "))
     nota3 = int(input("Nota de parte teorica: "))
 
-    resultado = nota1+nota2+nota3
-    print("La incidencia de la parte práctica en la nota es:", (resultado*0.1))
-    print("La incidencia de la parte problemática en la nota es:", (resultado*0.5))
-    print("La incidencia de la parte teórica en la nota es:", (resultado*0.4))
-
+    if (nota1 <= 0) or (nota2 <= 0) or (nota3 <= 0):
+        print("Error.")
+    else:
+        resultado = nota1+nota2+nota3
+        print("La incidencia de la parte práctica en la nota es:", (resultado*0.1))
+        print("La incidencia de la parte problemática en la nota es:", (resultado*0.5))
+        print("La incidencia de la parte teórica en la nota es:", (resultado*0.4))
+    
     nombreAlumno = str(input("Ingrese el nombre del siguiente alumno: "))
