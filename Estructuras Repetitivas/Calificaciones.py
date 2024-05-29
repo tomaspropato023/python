cc62c96b555f43777832197786acea250adc76ce
Tomas <166726520+tomaspropato023@users.noreply.github.com>
2024-05-29 11:48:04 -0300

    Create Calificaciones.py

---
+++ Estructuras Repetitivas/Calificaciones.py
@@ -0,0 +1,19 @@
+#EJ Calificaciones
+
+nombreAlumno = str(input("Ingrese el nombre del alumno: "))
+nota1 = 0
+nota2 = 0
+nota3 = 0
+resultado = 0
+
+while (nombreAlumno != ""):
+    nota1 = int(input("Nota de parte practica: "))
+    nota2 = int(input("Nota de problemas: "))
+    nota3 = int(input("Nota de parte teorica: "))
+
+    resultado = nota1+nota2+nota3
+    print("La incidencia de la parte práctica en la nota es:", (resultado*0.1))
+    print("La incidencia de la parte problemática en la nota es:", (resultado*0.5))
+    print("La incidencia de la parte teórica en la nota es:", (resultado*0.4))
+
+    nombreAlumno = str(input("Ingrese el nombre del siguiente alumno: "))
